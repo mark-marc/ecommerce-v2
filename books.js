29d24eb84607fe2eeee125f5a9eea21b126ee879
mark-marc <marcmedalle@hotmail.com>
2022-08-24 14:20:00 +1000

    Add books using inner html js

--- books.js
+++ books.js
@@ -1,3 +1,93 @@
+function renderBooks(filter) {
+  const booksWrapper = document.querySelector(".books")
+
+
+  const books = getBooks();
+
+
+  if (filter === 'LOW_TO_HIGH') {
+    
+ books.sort((a, b) => a.originalPrice- b.originalPrice);
+    
+  } 
+  else if (filter === 'HIGH_TO_LOW'){
+    books.sort((a, b) => b.originalPrice- a.originalPrice);
+
+  }
+  else if (filter === 'RATING'){
+    books.sort((a, b) => b.rating - a.rating);
+  }
+  
+  
+  const booksHtml = books
+  .map(book => {
+    return  `<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src="${book.url}"alt="">
+    </figure>
+    <div class="book__title">
+      ${book.title}
+    </div>
+    <div class="book__ratings">
+     ${ratingsHTML(book.rating)}
+    </div>
+    <div class="book__price">
+    ${priceHTML(book.originalPrice, book.salePrice)}
+     
+    </div>
+  </div>`;
+    
+  })
+  .join("");
+
+ 
+
+  booksWrapper.innerHTML = booksHtml;
+  
+}
+
+function priceHTML (originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  }
+  else {
+    return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span>$${salePrice.toFixed(2)}`
+
+  }
+  return 'there is a sale'
+  console.log(originalPrice, salePrice)
+
+}
+
+
+function ratingsHTML(rating) {
+  let ratingHTML = "";
+  
+  
+  for (let i = 0; i < Math.floor(rating); ++i){
+    ratingHTML += '<i class="fas fa-star"></i>\n';
+  }
+
+  if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fas fa-star-half-alt"></i>\n';
+  }
+
+  return ratingHTML;
+
+
+}
+function filterBooks(event) {
+  
+  renderBooks(event.target.value);
+  
+}
+
+setTimeout(() =>{
+  renderBooks();
+
+
+});
+
 
 
 // FAKE DATA
